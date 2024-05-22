dbbb068f7f836862153f90e347e6e99b6697c7ba
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-05-22 21:18:30 +0530

    jksvcksd

--- django_project/settings.py
+++ django_project/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'haldb',
-        'USER':"singh912",
-        'PASSWORD':'123456789!',
-        'HOST':'localhost',
-        'PORT':5432,
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'haldb',
+#         'USER':"singh912",
+#         'PASSWORD':'123456789!',
+#         'HOST':'localhost',
+#         'PORT':5432,
 
-    }
-}
-DATABASES['default']=dj_database_url.parse("postgres://singh912:4p0lXbVnIBHW1pRgkNdT11E1dhI30Yc2@dpg-cp70sja0si5c73am6acg-a.virginia-postgres.render.com/haldb")
+#     }
+# }
+DATABASES={'default':dj_database_url.parse("postgres://singh912:4p0lXbVnIBHW1pRgkNdT11E1dhI30Yc2@dpg-cp70sja0si5c73am6acg-a.virginia-postgres.render.com/haldb")}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
