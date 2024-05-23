dca39ad753c6553cba8ed799d0b2367c65d90020
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-05-24 01:13:47 +0530

    upda

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -3,13 +3,13 @@ from .import views
 urlpatterns = [
     path('index',views.index,name='index'),
     path('showall/',views.showall,name='showall'),
-    path('home/',views.home,name='home'),
+    path('',views.home,name='home'),
     path("data/",views.data,name='data'),
     path('delete/<int:contact_id>/', views.delete, name='delete'),
     path('delete_data/<int:data_id>/', views.delete_data, name='delete_data'),
     path('delete_insta/<int:insta_id>/', views.delete_insta, name='delete_insta'),
 
-    path("",views.insta,name='insta'),
+    path("insta/",views.insta,name='insta'),
 
 
 
